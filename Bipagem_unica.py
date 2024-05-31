ca703eaf9f953b1d7a6b7b18007404763417ca11
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-05-31 19:24:38 -0300

    Verção 0.6

--- Bipagem_unica.py
+++ Bipagem_unica.py
@@ -1,3 +1,4 @@
+import datetime
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QSpinBox, QListWidget, QFileDialog
 import pyautogui
@@ -7,6 +8,9 @@ import pygetwindow as gw
 import winsound
 import keyboard
 import os
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from PyQt5.QtWidgets import QFileDialog
 
 class MouseCoordinateApp(QWidget):
     def __init__(self):
@@ -14,6 +18,20 @@ class MouseCoordinateApp(QWidget):
         self.setWindowTitle("Bipagem unica - C.E.O.S")
 
         layout = QVBoxLayout()
+
+        self.layout_nome = QHBoxLayout()
+        self.nome_label = QLabel("Nome de Funcionario:")
+        self.layout_nome.addWidget(self.nome_label)
+        self.nome_input = QLineEdit()
+        self.layout_nome.addWidget(self.nome_input)
+        layout.addLayout(self.layout_nome)
+
+        self.layout_entregador = QHBoxLayout()
+        self.entregador_label = QLabel("Nome de Entregador:")
+        self.layout_entregador.addWidget(self.entregador_label)
+        self.entregador_input = QLineEdit()
+        self.layout_entregador.addWidget(self.entregador_input)
+        layout.addLayout(self.layout_entregador)
 
         self.label = QLabel("Clique nos botões para definir a posição do mouse:                    ")
         layout.addWidget(self.label)
@@ -39,18 +57,11 @@ class MouseCoordinateApp(QWidget):
         self.entry = QLineEdit()
         layout.addWidget(self.entry)
 
-        self.counter_label = QLabel("Contador: 0")
-        layout.addWidget(self.counter_label)
-
-        self.reset_button = QPushButton("Resetar Contador")
-        self.reset_button.clicked.connect(self.reset_counter)
-        layout.addWidget(self.reset_button)
-
         tempo_layout = QHBoxLayout()
         self.label_tempo1 = QLabel("Tempo de colagem (s):")
         tempo_layout.addWidget(self.label_tempo1)
         self.tempo1_spinbox = QSpinBox()
-        self.tempo1_spinbox.setRange(100, 5000)  # Valores em milissegundos
+        self.tempo1_spinbox.setRange(0, 5000) 
         tempo_layout.addWidget(self.tempo1_spinbox)
         layout.addLayout(tempo_layout)
 
@@ -58,7 +69,7 @@ class MouseCoordinateApp(QWidget):
         self.label_tempo2 = QLabel("Tempo de press enter:")
         tempo_layout.addWidget(self.label_tempo2)
         self.tempo2_spinbox = QSpinBox()
-        self.tempo2_spinbox.setRange(100, 5000)  # Valores em milissegundos
+        self.tempo2_spinbox.setRange(0, 5000)
         tempo_layout.addWidget(self.tempo2_spinbox)
         layout.addLayout(tempo_layout)
 
@@ -66,9 +77,12 @@ class MouseCoordinateApp(QWidget):
         self.label_tempo3 = QLabel("Tempo do mouse:")
         tempo_layout.addWidget(self.label_tempo3)
         self.tempo3_spinbox = QSpinBox()
-        self.tempo3_spinbox.setRange(500, 5000)  # Valores em milissegundos
+        self.tempo3_spinbox.setRange(0, 5000)
         tempo_layout.addWidget(self.tempo3_spinbox)
         layout.addLayout(tempo_layout)
+
+        self.counter_label = QLabel("Contador: 0")
+        layout.addWidget(self.counter_label)
 
         self.codigos_list_widget = QListWidget()
         layout.addWidget(self.codigos_list_widget)
@@ -81,6 +95,9 @@ class MouseCoordinateApp(QWidget):
         self.reset_list_button.clicked.connect(self.resetar_lista)
         layout.addWidget(self.reset_list_button)
 
+        self.messagem = QLabel("Defina as posicoes dos mouse e aperte Enter.")
+        layout.addWidget(self.messagem)
+
         self.setLayout(layout)
 
         self.positions = {}
@@ -92,14 +109,14 @@ class MouseCoordinateApp(QWidget):
         self.update_codigos_list_widget()
 
     def set_position1(self):
-        print("Posicione o mouse e pressione Enter.")
+        self.messagem.setText("Posicione o mouse e pressione Enter.")
         keyboard.wait("enter")
         x, y = pyautogui.position()
         self.positions['pos1'] = (x, y)
         self.coord_label1.setText(f"Posição 1: ({x}, {y})")
 
     def set_position2(self):
-        print("Posicione o mouse e pressione Enter.")
+        self.messagem.setText("Posicione o mouse e pressione Enter.")
         keyboard.wait("enter")
         x, y = pyautogui.position()
         self.positions['pos2'] = (x, y)
@@ -113,6 +130,8 @@ class MouseCoordinateApp(QWidget):
 
     def salvar_codigo(self, codigo):
         with open("codigos_inseridos.txt", "a") as file:
+            if not file.tell():  
+                file.write("\n") 
             file.write(f"{codigo}\n")
 
     def update_codigos_list_widget(self):
@@ -120,55 +139,67 @@ class MouseCoordinateApp(QWidget):
         self.codigos_list_widget.addItems(self.codigos_inseridos)
 
     def start_inserir_codigo(self):
-        if 'pos1' in self.positions and 'pos2' in self.positions:
+        if 'pos1' in self.positions and 'pos2' in self.positions and self.nome_input.text() != '' and self.entregador_input.text() != '':
             codigo_barras = self.entry.text()
             if len(codigo_barras) < 1:
-                print("Código de barras inválido. Insira um código com pelo menos 1 caractere.")
+                self.messagem.setText("Código de barras inválido. \nInsira um código com pelo menos 1 caractere.")
                 return
 
             if codigo_barras in self.codigos_inseridos:
                 self.entry.clear()
                 self.bring_to_front()
                 winsound.Beep(820, 1000)
-                print("Código de barras já inserido.")
+                self.messagem.setText("Código de barras já inserido.")
                 return
 
             self.codigos_inseridos.add(codigo_barras)
             self.salvar_codigo(codigo_barras)
+            
 
-            tempo1 = self.tempo1_spinbox.value() / 1000 
-            tempo2 = self.tempo2_spinbox.value() / 1000 
-            tempo3 = self.tempo3_spinbox.value() / 1000 
+            tempo1 = self.tempo1_spinbox.value() / 1000  
+            tempo2 = self.tempo2_spinbox.value() / 1000  
+            tempo3 = self.tempo3_spinbox.value() / 1000  
 
             inserir_codigo(codigo_barras, *self.positions['pos1'], *self.positions['pos2'], tempo1, tempo2, tempo3)
-            self.entry.clear()  
-
+            self.entry.clear() 
             self.counter += 1
             self.counter_label.setText(f"Contador: {self.counter}")
-
             self.bring_to_front()
-
             winsound.Beep(3520, 250)
             self.update_codigos_list_widget()
         else:
-            print("Por favor, defina todas as posições antes de iniciar.")
+            self.messagem.setText("Por favor, defina todas as posições, nome e entregador\nantes de iniciar.")
 
     def bring_to_front(self):
         window = gw.getWindowsWithTitle(self.windowTitle())[0]
         if window:
             window.activate()
 
-    def reset_counter(self):
-        self.counter = 0
-        self.counter_label.setText(f"Contador: {self.counter}")
-
     def exportar_lista(self):
         options = QFileDialog.Options()
-        file_path, _ = QFileDialog.getSaveFileName(self, "Salvar Lista", "", "Text Files (*.txt);;All Files (*)", options=options)
+        file_path, _ = QFileDialog.getSaveFileName(self, "Salvar Lista", "", "PDF Files (*.pdf);;All Files (*)", options=options)
+    
         if file_path:
-            with open(file_path, "w") as file:
-                for codigo in self.codigos_inseridos:
-                    file.write(f"{codigo}\n")
+            c = canvas.Canvas(file_path, pagesize=letter)
+            
+            now = datetime.datetime.now()
+            formatted_now = now.strftime("%Y-%m-%d %H:%M:%S")
+            
+            c.drawString(100, 750,"Hora e Dia: " + formatted_now)
+            c.drawString(100, 735,"Funcionario:" + self.nome_input.text())
+            c.drawString(100, 720,"Entregador:" + self.entregador_input.text())
+            c.drawString(100, 705,"Contagem:" + self.counter_label.text())
+            c.drawString(100, 690,"Confirme as informações")
+            c.drawString(100, 670,"Codigos inseridos:")
+
+            y = 665  
+            for codigo in self.codigos_inseridos:
+                c.drawString(100, y, str(codigo))
+                y -= 12 
+    
+            c.save()
+            
+        self.resetar_lista()
 
     def resetar_lista(self):
         self.codigos_inseridos.clear()
