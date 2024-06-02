af911eceacc63126b0013ef4dc54c0ffd40d993e
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2024-06-02 04:01:56 -0300

    Verção 0.7.1Beta

--- Bipagem_unica.py
+++ Bipagem_unica.py
@@ -136,7 +136,7 @@ class MouseCoordinateApp(QWidget):
         layout.addWidget(self.messagem)
 
         self.ceos_label_layout = QHBoxLayout()
-        self.Ceos = QLabel("C.E.O.S - 0.7.1b LC-transporte")
+        self.Ceos = QLabel("C.E.O.S - 0.7.1beta LC-transporte")
         self.Ceos.setStyleSheet("color: gray;") 
         self.ceos_label_layout.addWidget(self.Ceos)
         self.ceos_label_layout.setAlignment(Qt.AlignRight)
